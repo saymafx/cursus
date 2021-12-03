2d6669859cc3ad8f877f06e2b3bf50dde532b951
saymafx <93143559+saymafx@users.noreply.github.com>
2021-12-03 16:55:58 +0100

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,52 @@
 /*   By: tidigov <tidigov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:56:12 by tidigov           #+#    #+#             */
-/*   Updated: 2021/11/08 13:56:41 by tidigov          ###   ########.fr       */
+/*   Updated: 2021/12/03 16:54:51 by tidigov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
+#include "get_next_line.h"
+
+char	*ft_first_line(int fd, char *result)
 {
+	int     bytelu;
+    char    buf[BUFFER_SIZE + 1];
+    result = ft_strdup("");
+
+	bytelu = read(fd, buf, BUFFER_SIZE);
+    while (bytelu > 0)
+    {
+        buf[bytelu] = '\0';
+        result = ft_strjoin(result, buf);
+        bytelu = read(fd, buf, BUFFER_SIZE);
+    }
+    return (result);
+}
+
+char	*ft_cleanline(char *cleanresult)
+{
+	int     a;
+	char	*first_line;
+    cleanresult = ft_strdup("");
+
+    a = 0;
+	first_line = *ft_first_line(*result);
+    if (!first_line)
+        return (NULL);
+    while (first_line[a++] != '\n')
+		cleanresult = ft_strjoin(cleanresult, first_line);
+	return (cleanresult);
+}
+
+//char	*get_next_line(int fd)
+
+int main(int argc, char **argv)
+{
+    int	fd;
     
+    (void) argc;
+    fd = open(argv[1], O_RDONLY);
+    printf("%s", *ft_cleanline(fd));
+    close(fd);
+    return(0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: tidigov <tidigov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 15:26:57 by tidigov           #+#    #+#             */
-/*   Updated: 2021/11/09 15:29:01 by tidigov          ###   ########.fr       */
+/*   Updated: 2021/12/03 16:54:20 by tidigov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 3
+#endif
 
 # include <stdlib.h>
 # include <string.h>
@@ -19,7 +22,22 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <errno.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+#include <fcntl.h>
 
-char	*get_next_line(int fd);
+//char	*get_next_line(int fd);
+char	*ft_cleanline(char *cleanresult);
+char	*ft_first_line(int fd, char *result);
+
+//libft
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
