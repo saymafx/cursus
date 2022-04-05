03ba7ec753ef8c458cef0aadc7af1097bfe0f5de
saymafx <93143559+saymafx@users.noreply.github.com>
2022-04-05 13:01:57 +0100

    Update parsing.c

--- so_long/parsing.c
+++ so_long/parsing.c
@@ -40,3 +40,41 @@ void    co_ex(char **tab, t_jojo *map)
         exit(EXIT_FAILURE);
     }
 }
+
+static void check_move_down(t_jojo *map)
+{
+    if (check_move(map, map->nbr_y, map->nbr_x) == 3)
+    {
+        map->collectible--;
+        map->tab[map->nbr_y][map->nbr_x] = 'P';
+        map->tab[map->nbr_y - 1][map->nbr_x] = '0';
+    }
+    else if (check_move(map, map->nbr_y, map->nbr_x) == 2 && !map->collectible)
+    {
+        free_tab(map->tab);
+        exit(EXIT_SUCCESS);
+    }
+    if (map->tab[map->nbr_y - 1][map->nbr_x] != 'E' && map->tab[map->nbr_y][map->nbr_x] != 'E')
+	{
+		map->tab[map->nbr_y][map->nbr_x] = 'P';
+		map->tab[map->nbr_y - 1][map->nbr_x] = '0';
+	}
+    map->bg_player = mlx_xpm_file_to_image(map->mlx, map->player_down, &map->win_width, &map->win_height);
+}
+
+void	player_down(t_data *map)
+{
+	map->nbr_y++;
+	if (check_map_for_move(map, map->nbr_y, map->nbr_x) == 2
+		&& map->collectible)
+		map->nbr_y--;
+	else if (check_map_for_move(map, map->nbr_y, map->nbr_x) == 0)
+		map->nbr_y--;
+	else if (check_map_for_move(map, map->nbr_y, map->nbr_x))
+	{
+		ft_bas_extension(map);
+		parsing_map(map->tab, map);
+		map->move++;
+		print_info(map);
+	}
+}
