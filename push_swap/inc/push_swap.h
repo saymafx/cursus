a5de733bdf7bb6fab78fcffb82dda4e56f5614ce
saymafx <93143559+saymafx@users.noreply.github.com>
2022-04-29 08:14:46 +0100

    Add files via upload

---
+++ push_swap/inc/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tidigov <tidigov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 17:28:30 by tidigov           #+#    #+#             */
+/*   Updated: 2022/04/27 17:33:21 by tidigov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <mlx.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+# include <string.h>
+
+#endif
